e1d334ad20ae1011a7f55d62f6d72f25b6637e4a
Guilherme Dias <guilhermeaugusto.dias@gmail.com>
2017-09-22 19:27:46 -0300

    Normalize issue data

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import JiraClient from './jira/client'
 import JiraService from './jira/service'
 
+import toLowerCase from './pipeline/stage/to-lower-case'
+import {Pipeline} from './pipeline/pipeline'
+
 let jiraClient = new JiraClient('https://jira.atlassian.com/rest/api/latest')
 let jiraService = new JiraService(jiraClient)
 
 jiraService.fetchIssue('JRA-9')
   .then((issue) => {
-    console.log(issue)
+    let normalizationPipeline = new Pipeline()
+      .pipe(toLowerCase)
+
+    let normalizedSummary = normalizationPipeline.process(issue.summary)
+    let normalizedDescription = normalizationPipeline.process(issue.description)
+
+    console.log(`Summary: ${normalizedSummary}`)
+    console.log(`Description: ${normalizedDescription}`)
   })
